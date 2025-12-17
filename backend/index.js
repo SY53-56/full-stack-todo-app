463231b7dba920864dae28a97a062f32a519dea5
sahul-yadav <yadavsahul220@gmail.com>
2025-12-17 18:51:33 +0530

    port delete form env file

--- backend/index.js
+++ backend/index.js
@@ -32,5 +32,5 @@ app.get("/sahul", (req, res) => {
   res.send("Backend working ✅");
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
